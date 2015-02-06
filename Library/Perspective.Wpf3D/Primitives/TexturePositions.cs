16f90da93ca336b63b99367fba485cda0ad846ee
odewit_cp <SND\odewit_cp>
2015-02-06 14:58:46 +0000

    idem

---
+++ Library/Perspective.Wpf3D/Primitives/TexturePositions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Perspective.Wpf3D.Primitives
+{
+    /// <summary>
+    /// An enumeration to identify the position of a texture (i.e. for a Bar3D).
+    /// </summary>
+    public enum TexturePositions
+    {
+        /// <summary>
+        /// The texture is applied on the ends of the model.
+        /// </summary>
+        OnEnds,
+
+        /// <summary>
+        /// The texture is applied on the sides of the model.s
+        /// </summary>
+        OnSides
+    }
+}
